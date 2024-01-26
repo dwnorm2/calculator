d378fc8287b9a930a89a29c770ffe92404086ba1
Dan <dwnorm2@gmail.com>
2024-01-26 11:45:15 -0600

    create calc object and html elements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
+class Calculator {
+  constructor() {
+    this.current = 0;
+  }
 
+  inputNum(num) {
+    this.current += num;
+  }
+
+  add() {
+    this.current += "+";
+  }
+
+  subtract() {
+    this.current += "-";
+  }
+
+  multiply() {
+    this.current += "*";
+  }
+
+  divide() {
+    this.current += "/";
+  }
+
+  equals() {
+    console.log(eval(this.current));
+    this.current = eval(this.current);
+  }
+
+  decimal() {
+    this.current += ".";
+  }
+
+  clear() {
+    this.current = 0;
+  }
+}
+
+let calc = new Calculator();
